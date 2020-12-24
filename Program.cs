0f7c684a60b77dafdea59048bd02b7694738e09d
Gabriel <gaplogan@gmail.com>
2020-12-24 16:31:52 -0300

    2020.12.24.16.31

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace cs_1._9_AvisoFesta2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declara variável inteira para controle do loop
+            int j = 1; //inicializa variável de controle do loop
+            
+            while (j <= 5)
+            {//início do loop
+                //escreve a frase
+                Console.WriteLine("Hoje vai ter festa na casa da Fabi");
+                //incrementa variável de controle do loop (contador)
+                j++; // Mesmo que j = j + 1;
+            }//fim do loop            
+        }
+    }
+}
